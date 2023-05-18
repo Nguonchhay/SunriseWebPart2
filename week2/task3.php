6cea2cc0fe56004c80f51ffb8a414a7dce4e46a9
Nguonchhay Touch <nguonchhay.touch@gmail.com>
2023-05-18 19:48:29 +0700

    [WEEK2] Task3 practice

---
+++ week2/task3.php
@@ -0,0 +1,48 @@
+<!doctype html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>Task3 | Week 2</title>
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+    </head>
+    <body>
+        <div class="container">
+            <div class="col-6">
+                <form action="" method="POST" class="mt-5">
+                    <h1>Number Checking</h1>
+                    <div class="mb-3">
+                        <label for="value1" class="form-label">Value 1</label>
+                        <input type="number" name="value1" class="form-control" id="value1">
+                    </div>
+
+                    <div class="mb-3">
+                        <label for="value2" class="form-label">Value 2</label>
+                        <input type="number" name="value2" class="form-control" id="value2">
+                    </div>
+                 
+                    <button type="submit" class="btn btn-primary">Submit</button>
+                </form>
+
+                <div class="mb-3">
+                    <?php
+                        $message = '';
+                        if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+                            $value1 = $_POST['value1'];
+                            $value2 = $_POST['value2']; 
+
+                            if ($value1 == 30 || $value2 == 30 || $value1 + $value2 == 30) {
+                                $message = 'True';
+                            } else {
+                                $message = 'False';
+                            }
+                        }                      
+                    ?>
+                    <p>Result : <?=$message ?></p>
+                </div>
+            </div>
+        </div>
+
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
+    </body>
+</html>
