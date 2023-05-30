0762a81e0e955ad021df21e6bb376b90a544a706
Nguonchhay Touch <nguonchhay.touch@gmail.com>
2023-05-30 18:57:39 +0700

    [IMP] Refactor menu rendering

--- servicy.com/partials/page.php
+++ servicy.com/partials/page.php
@@ -1,32 +1,59 @@
 <?php
 
+$menus = [
+    [
+        'key' => 'home',
+        'label' => 'Home',
+        'link' => '/sunrise-web2/servicy.com'
+    ],
+    [
+        'key' => 'service',
+        'label' => 'Services',
+        'link' => 'service.php'
+    ],
+    [
+        'key' => 'portfolio',
+        'label' => 'Portfolios',
+        'link' => 'portfolio.php'
+    ],
+    [
+        'key' => 'about',
+        'label' => 'About',
+        'link' => 'about.php'
+    ],
+    [
+        'key' => 'team',
+        'label' => 'Team',
+        'link' => 'team.php'
+    ],
+    [
+        'key' => 'contact',
+        'label' => 'Contact Us',
+        'link' => 'contact.php'
+    ]
+];
+
+function renderMenuItem($key, $label, $link, $curPage) {
+    return '
+        <li class="nav-item">
+            <a class="nav-link' . ($curPage === $key ? ' active' : '') . '" href="' . $link . '">' . $label . '</a>
+        </li>
+    ';
+}
+
 function renderMenu($curPage = 'home') {
+    $menuItems = '';
+    foreach($GLOBALS['menus'] as $menu) {
+        $menuItems .= renderMenuItem($menu['key'], $menu['label'], $menu['link'], $curPage);
+    }
+
     return '
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
         Menu
             <i class="fas fa-bars ms-1"></i>
         </button>
         <div class="collapse navbar-collapse" id="navbarResponsive">
-            <ul class="navbar-nav text-uppercase ms-auto py-4 py-lg-0">
-                <li class="nav-item">
-                    <a class="nav-link' . ($curPage === 'home' ? ' active' : '') . '" href="/sunrise-web2/servicy.com">Home</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link' . ($curPage === 'service' ? ' active' : '') . '" href="service.php">Services</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link' . ($curPage === 'portfolio' ? ' active' : '') . '" href="portfolio.php">Portfolio</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link' . ($curPage === 'about' ? ' active' : '') . '" href="about.php">About</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link' . ($curPage === 'team' ? ' active' : '') . '" href="team.php">Team</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link' . ($curPage === 'contact' ? ' active' : '') . '" href="contact.php">Contact</a>
-                </li>
-            </ul>
+            <ul class="navbar-nav text-uppercase ms-auto py-4 py-lg-0">' . $menuItems . '</ul>
         </div>
     ';
 }
