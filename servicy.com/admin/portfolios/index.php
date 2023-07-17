d6dad329485e9d5352530d830e4b96d4a5db8790
Nguonchhay Touch <nguonchhay@web-essentials.co>
2023-07-17 19:48:05 +0700

    [TASK] Add full url in add new portfolio

--- servicy.com/admin/portfolios/index.php
+++ servicy.com/admin/portfolios/index.php
@@ -226,7 +226,7 @@ $headerTitle = 'Portfolios';
                     <!-- Page Heading -->
                     <div class="d-sm-flex align-items-center justify-content-between mb-4">
                         <h1 class="h3 mb-0 text-gray-800">Portfolios</h1>
-                        <a href="/admin/portfolios/create.php" class="d-none d-sm-inline-block btn btn-sm btn-primary shadow-sm"><i
+                        <a href="<?=BASE_URL?>/admin/portfolios/create.php" class="d-none d-sm-inline-block btn btn-sm btn-primary shadow-sm"><i
                                 class="fas fa-plus fa-sm text-white-50"></i> Add New</a>
                     </div>
 
